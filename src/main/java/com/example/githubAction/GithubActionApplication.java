3ad8fb1a2e4ab438e7d26472ab5512cd15eabc9e
Vishnu <jadhavvishnu666@.com>
2024-04-30 11:20:22 +0530

    Fu

--- src/main/java/com/example/githubAction/GithubActionApplication.java
+++ src/main/java/com/example/githubAction/GithubActionApplication.java
@@ -8,7 +8,7 @@ public class GithubActionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GithubActionApplication.class, args);
-		System.out.println("Hello");
+		System.out.println("Fuck");
 	
 	}
 
