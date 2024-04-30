89ee118965b54c051cb1cf5d94c2372714db815c
vishnu18032 <112427746+vishnu18032@users.noreply.github.com>
2024-04-30 10:56:53 +0530

    Update GithubActionApplication.java

--- src/main/java/com/example/githubAction/GithubActionApplication.java
+++ src/main/java/com/example/githubAction/GithubActionApplication.java
@@ -9,6 +9,7 @@ public class GithubActionApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GithubActionApplication.class, args);
 		System.out.println("Hello");
+		whoaho
 	}
 
 }
